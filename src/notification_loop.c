2d80221ec34a0dc8a760b68d76c29299f11374b6
HardDie <oleg1995sysoev@yandex.ru>
2019-01-26 16:50:50 +0700

    Fix typo

--- src/notification_loop.c
+++ src/notification_loop.c
@@ -26,7 +26,7 @@ static void* main_loop(void *arg) {
 		// Assemble json string
 		pthread_mutex_lock(&vars->mutex);
 		json_object_object_add(obj, "Packet count", json_object_new_int(vars->count));
-		json_object_object_add(obj, "Recieve bytes", json_object_new_int(vars->recv_bytes));
+		json_object_object_add(obj, "Receive bytes", json_object_new_int(vars->recv_bytes));
 		pthread_mutex_unlock(&vars->mutex);
 
 		msg = json_object_to_json_string(obj);
